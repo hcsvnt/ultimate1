d6443ec0da7b887827c0bc959c5b58f2d449620c
Hallala <mateuszhallala@gmail.com>
2021-09-11 15:08:28 +0200

    added router and set up the routing, albeit without params etc

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 // import styles from './global.module.css';
 import Layout from './components/layout/layout';
@@ -6,15 +7,53 @@ import Register from './components/Register/Register';
 import ListsDisplay from './components/lists-display/ListsDisplay';
 import TodoList from './components/todo-list/TodoList';
 
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+// function App() {
+//   return (
+//     <div>
+//       <Layout>
+//         <Login />
+//         <Register />
+//         <ListsDisplay/>
+//         <TodoList />
+//       </Layout>
+//     </div>
+//   );
+// }
+
 function App() {
   return (
     <div>
-      <Layout>
-        <Login />
-        <Register />
-        <ListsDisplay/>
-        <TodoList />
-      </Layout>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+              <h1>
+                Welcome Home, Sir!
+              </h1>
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/lists">
+              <ListsDisplay/>
+            </Route>
+            <Route path="/list">
+              <TodoList />
+            </Route>
+          </Switch>
+        </Layout>
+      </Router>
     </div>
   );
 }
